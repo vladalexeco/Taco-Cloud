acf83db808033d78dae319355215bd1cdfea1dff
user <user@vabolshakov-win>
2024-09-16 17:23:49 +0300

    Created Database with JdbcTemplate

---
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -0,0 +1,50 @@
+package tacos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import jakarta.validation.Valid;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import tacos.TacoOrder;
+import tacos.data.OrderRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+	
+	OrderRepository orderRepo;
+	
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+	
+	@GetMapping("/current")
+	public String orderForm() {
+		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(@Valid TacoOrder order, Errors errors, SessionStatus sessionStatus) {
+		
+		if (errors.hasErrors()) {
+			return "orderForm";
+		}
+		
+		orderRepo.save(order);
+		
+		log.info("Taco order {}", order);
+		
+		sessionStatus.setComplete();
+		
+		return "redirect:/";
+
+	}
+}
